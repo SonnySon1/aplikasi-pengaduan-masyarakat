0211fda849213938662a4f3665f38b98a4251a01
SonnySon1 <sonysudrajat94@gmail.com>
2023-08-30 15:44:39 +0700

    memperbaiki foto di bagian logo side bar

--- resources/views/layouts/admin/navbar/navbar.blade.php
+++ resources/views/layouts/admin/navbar/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="nav-header">
     <a href="index.html" class="brand-logo">
-        <img class="logo-abbr" src="dist/images/logo.png" alt="">
-        <img class="logo-compact" src="dist/images/logo-text.png" alt="">
-        <img class="brand-title" src="dist/images/logo-text.png" alt="">
+        <img class="logo-abbr" src="{{ asset('dist/images/logo.png') }}" alt="">
+        <img class="logo-compact" src="{{ asset('dist/images/logo-text.png') }}" alt="">
+        <img class="brand-title" src="{{ asset('dist/images/logo-text.png') }}" alt="">
     </a>
 
     <div class="nav-control">
